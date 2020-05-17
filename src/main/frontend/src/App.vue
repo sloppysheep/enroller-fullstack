6d0c17b341d7face88dbcef0dac88cf59054c15f
Aleksandra Wandzilak <aleksandra.wandzilak@gmail.com>
2020-05-17 14:24:17 +0200

    frontend and backend finally connected

--- src/main/frontend/src/App.vue
+++ src/main/frontend/src/App.vue
@@ -11,7 +11,20 @@
       <meetings-page :username="authenticatedUsername"></meetings-page>
     </div>
     <div v-else>
-      <login-form @login="login($event)"></login-form>
+	  <button @click="registering = false"
+	          :class="registering ? 'button-outline' : ''">Loguję się</button>
+	  <button @click="registering = true"
+	          :class="!registering ? 'button-outline' : ''">Rejestruję się</button>
+	          
+	  <div v-if="error" class="error-alert">{{error}}</div>
+	
+      <login-form @login="login($event)"
+	              v-if="registering == false"></login-form>
+	  
+	  <login-form @login="register($event)"
+	              v-else
+	              button-label="Zarejestruj się"></login-form>
+	  
     </div>
   </div>
 </template>
@@ -25,12 +38,24 @@
         components: {LoginForm, MeetingsPage},
         data() {
             return {
-                authenticatedUsername: ""
+                authenticatedUsername: "",
+                registering: false,
+                error: ''
             };
         },
         methods: {
             login(user) {
                 this.authenticatedUsername = user.login;
+            },
+            register(user) {
+            	 this.error = '';
+            	 this.$http.post('participants', user)
+            	     .then(response => {
+            	     	this.registering = false;
+            	     })
+            	     .catch(response => {
+               	    	this.error="Nazwa użytkownika jest zajęta";     
+            	     });
             },
             logout() {
                 this.authenticatedUsername = '';
@@ -47,6 +72,15 @@
 
   .logo {
     vertical-align: middle;
+  }
+  
+  .error-alert{
+  border: 3px dotted red;
+  padding: 10px;
+  background: pink;
+  text-align: center;
+  border-radius: 50%;
+  
   }
 </style>
 
